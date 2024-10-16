a431b5befca5c270eff732c84d3a65c4ccac14aa
crislmfroes <cris.lima.froes@gmail.com>
2024-10-16 14:02:47 -0300

    Init node before testing skill

--- nodes/test_speak_skill_node.py
+++ nodes/test_speak_skill_node.py
@@ -3,5 +3,6 @@ from robot_interface.butia_behavior_interface import ButiaBehaviorInterface
 import rospy
 
 if __name__ == '__main__':
+    rospy.init_node('test_skill_node', anonymous=True)
     robot_interface = ButiaBehaviorInterface(fake_execution=False)
     assert robot_interface.speak(utterance='Hello world!') == 'succeeded'
