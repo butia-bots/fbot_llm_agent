e570174069c62bc4b4af5d39206f3ba1d3a8b853
crislmfroes <cris.lima.froes@gmail.com>
2024-10-16 14:00:04 -0300

    Add skill test nodes

---
+++ nodes/test_answer_skill_node.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from robot_interface.butia_behavior_interface import ButiaBehaviorInterface
+import rospy
+
+if __name__ == '__main__':
+    robot_interface = ButiaBehaviorInterface(fake_execution=False)
+    assert robot_interface.answer() == 'succeeded'
